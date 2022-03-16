89a2bffa73df38ac876d8319232e2ec4ef711e89
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-16 16:55:15 -0500

    changes to sku

--- sku-generator/src/App.js
+++ sku-generator/src/App.js
@@ -5,14 +5,16 @@ const App = () => {
 
   const [skuList, setSkuList] = useState([]);
 
+  //generate sku based on desired length and save to state. will change to take input for length of sku desired
   const generateNewSku = () => {
     const lengthOfSku = 9999;
     const newSku = Math.floor(Math.random() * lengthOfSku);
     console.log(newSku);
+    setSkuList(newSku);
   }
 
   generateNewSku();
-
+  console.log(skuList);
   return (
     <div className="App">
     </div>
