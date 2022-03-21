126d42f4aebbd767e08babfa3ce66111455a14f2
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-20 23:55:44 -0500

    change in variable scope

--- sku-generator/src/App.js
+++ sku-generator/src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 const App = () => {
 
   const [skuList, setSkuList] = useState([]);
+  // moved from generateNewFunction to global scope so can check newSku against those already saved/used
+  let newSku;
+  let lengthOfSku;
 
   //generate sku based on desired length and save to state. will change to take input for length of sku desired
   const generateNewSku = () => {
-    const lengthOfSku = 9999;
-    const newSku = Math.floor(Math.random() * lengthOfSku);
+    lengthOfSku = 9999;
+    newSku = Math.floor(Math.random() * lengthOfSku);
     //console.log(newSku);
     setSkuList([...skuList, newSku]);
   }
 
+  //check generated sku against those stored in state if in state already generate another sku
   const checkForExsistingSku = () => {
-    //check generated sku against those stored in state if in state already generate another sku
+    if ()
   };
 
   //only run function once on page load
