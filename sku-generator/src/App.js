230dd4c8fb85744b53450950656ee09422f50b0b
Juan Damian <59286923+damianjuan@users.noreply.github.com>
2022-03-30 23:58:40 -0500

    save logic but still need to modify

--- sku-generator/src/App.js
+++ sku-generator/src/App.js
@@ -29,7 +29,12 @@ const App = () => {
   //move formula here for saved skus. only show when requested
   const showSavedSkus = () => {
     console.log(`showSavedSkus function called`);
-
+    <h2>SKUs:</h2>
+    {
+      skuList.map((sku) => (
+        <h3>{sku}</h3>
+      ))
+    }
   };
 
 
@@ -45,7 +50,7 @@ const App = () => {
 
     <Container>
       <Paper elevation={4}>
-        <Button>show my saved SKUs</Button>
+        <Button variant="contained" onClick={showSavedSkus}>show my saved SKUs</Button>
         <Button variant="contained" onClick={generateNewSku}>New SKU</Button>
         <h1>New SKU: {newSku}</h1>
         {/* <h2>SKUs:</h2>
